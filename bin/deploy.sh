375f0103f9e54910657b5562142dd56bc81914e1
devowhippit <devonhirth@gmail.com>
2017-07-20 15:19:08 -0400

    working deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,49 +2,50 @@
 
 # Usage
 # Modify configuration file ../deploy.cfg
-# Then run `bin/deploy.sh <branch> <env> <optional message>`
+# Then run `bin/deploy.sh <env> <branch> <optional message>`
 
 source deploy.cfg
 BRANCH=$1
 ENV=$2
 MESSAGE="$3"
 COMMAND="git push $ENV $BRANCH:master --force"
-DOMAIN="http://${WPENGINE_NAME}.${ENV}.wpengine.com"
+DOMAIN="https://${WPENGINE_NAME}.${ENV}.wpengine.com"
 
 function branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
 
 function find_wp {
-  echo "";
+  echo ""
   echo "\xF0\x9F\x94\xAC  Finding application source...";
   if cd $WORDPRESS_DIR ; then
     echo "Found!"
   else
     echo "Couldn't find your application directory, be sure to add it to config.cfg"
-    echo "";
+    echo ""
     exit 0
   fi
 }
 
 function add_remote {
-  echo "";
+  echo ""
   echo "\xF0\x9F\x94\xAD  Adding remote..."
-  if git remote add $ENV git@git.wpengine.com:$ENV/$WPENGINE_NAME.git ; then
+  if git remote add $ENV git@git.wpengine.com:$ENV/$WPENGINE_NAME.git 2> /dev/null; then
     echo "Added"
   else
-    echo "It is not that fatal."
+    echo "Already present"
   fi
 }
 
 function git_push {
-  echo "";
+  echo ""
   echo "\xF0\x9F\x9A\x80  Git push...";
-  if $COMMAND ; then
-    post_slack
+  post_slack
+  if eval $COMMAND ; then
+    success
   else
     echo "Could not push to remote"
-    echo "";
+    echo ""
     exit 0
   fi
 }
@@ -54,35 +55,37 @@ function post_slack() {
   MESSAGE="@${SLACK_USER} deploying ${DOMAIN} via \`${COMMAND}\` ${MESSAGE}"
   PAYLOAD="payload={\"text\":\"${MESSAGE}\", \"channel\":\"${SLACK_CHANNEL}\", \"username\":\"${SLACK_USERNAME}\", \"icon_emoji\":\"${SLACK_ICON}\"}"
 
-  echo "";
+  echo ""
   echo "${SLACK_ICON_BYTE}  Alerting the team on ${SLACK_CHANNEL}...";
-  echo "";
+  echo ""
   echo "${SLACK_USERNAME}"
   echo $MESSAGE
   curl -X POST --data-urlencode ${PAYLOAD} ${SLACK_INCOMMING_WEBHOOK}
 
 }
 
-# function send_email() {
-#   # mail -s "The subject line" <email address or phone number address>
-# }
-
 function success() {
 
   MESSAGE="Deployment of ${DOMAIN} by @${SLACK_USER} complete via \`${COMMAND}\`"
   PAYLOAD="payload={\"text\":\"${MESSAGE}\", \"channel\":\"${SLACK_CHANNEL}\", \"username\":\"${SLACK_USERNAME}\", \"icon_emoji\":\"${SLACK_ICON}\"}"
 
-  echo "";
+  echo ""
   echo "\xF0\x9F\x99\x8C  Deployment of ${DOMAIN} complete."
-  echo "";
+  echo ""
+  echo "${SLACK_USERNAME}"
+  echo $MESSAGE
+  curl -X POST --data-urlencode ${PAYLOAD} ${SLACK_INCOMMING_WEBHOOK}
 
 }
+
+# function run_tests() {
+#   # test.sh
+# }
 
 IFS='%'
 find_wp
 add_remote
 git_push
-success
 unset IFS
 
 exit 0
