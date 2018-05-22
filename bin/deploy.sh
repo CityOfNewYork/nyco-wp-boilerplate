acff830bcad4a202d43fc90f2c1d30a2ae6656d6
kimpenguin <kimmypeng@gmail.com>
2018-05-22 15:53:45 -0400

    adding some headers for deployment types

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,6 +10,7 @@ SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # source the environmental variables
 source "${SCRIPT_PATH}/../.env"
+source "${SCRIPT_PATH}/headers.sh"
 source "${SCRIPT_PATH}/functions.sh"
 
 # create an array of all the projects the user has listed in .env
@@ -17,5 +18,24 @@ ARR=(${!PROJ_*})
 
 ###################
 # Execute the functions
+welcomeHead
+
 destProj
-echo $userProj
+echo "You selected:" $userProj
+
+echo "What do you want to do?"
+echo "[0] Deploy"
+echo "[1] Sync"
+echo "[2] Update"
+printf "Selection: "
+read selection
+if [[ $selection == 0 ]]; then
+	deployHead
+elif [[ $selection == 1 ]]; then
+	syncHead
+elif [[ $selection == 2 ]]; then
+	updateHead
+else
+	echo "Nothing was selected... exiting."
+	exit 1
+fi
