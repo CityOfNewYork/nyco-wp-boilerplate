d39cb906dab47a6f3c16db9bb59ffd88ac531248
kimpenguin <kimmypeng@gmail.com>
2018-09-24 09:56:45 -0400

    adding updateWP function

--- bin/boilerplate.sh
+++ bin/boilerplate.sh
@@ -8,8 +8,8 @@ BASE_PATH=$(dirname "$SCRIPT_PATH")
 source "${BASE_PATH}/config/wp.cfg"
 PROJ_PATH=$WP
 
-bpTempDir=$baseDir/temp/bp
-wpTempDir=$baseDir/temp/wp
+bpTempDir=$BASE_PATH/temp/bp
+wpTempDir=$BASE_PATH/temp/wp
 
 # function will move the .git/ and .gitignore of the WP
 # into the temporary folder so that you'll be able to 
@@ -17,7 +17,7 @@ wpTempDir=$baseDir/temp/wp
 function updateBoilerplate() {
 	echo "\nGreat! You've chosen update the boilerplate. We're gonna get that setup for you right now."
 	echo "If you made a mistake, hit Ctrl-C"
-	echo "\nMoving the boilerplate back to ${baseDir} from ${bpTempDir}"
+	echo "\nMoving the boilerplate back to ${BASE_PATH} from ${bpTempDir}\n"
 
 	# checking existence of repo in root
 	if [ -d $BASE_PATH/.git ] || [ -e $BASE_PATH/.gitignore ] ; then
@@ -27,22 +27,23 @@ function updateBoilerplate() {
 
 	# moves boilerplate repo to root
 	if [ ! -d $BASE_PATH/.git ] && [ -d $bpTempDir/.git ]; then
-		echo "moving the boilerplate repo to the root"
+		echo "Moving the boilerplate repo to the root"
 		mv $bpTempDir/.git/ $BASE_PATH
 		if [ -e $bpTempDir/.gitignore ]; then
 	    echo "Moving the gitignore."
 	    mv $bpTempDir/.gitignore $BASE_PATH
 		else
-	    echo "No gitignore."
+	    echo "No gitignore. Nothing to move."
 		fi
+		echo ">>>DONE!"
 	fi
 
 	# move the project repo to temp
-	if [ -d $PROJ_PATH/.git ] || [ -e $BASE_PATH/.gitignore ] ; then
+	if [ -d $PROJ_PATH/.git ] || [ -e $PROJ_PATH/.gitignore ] ; then
 		echo "\nMoving the WordPress project repo to ${wpTempDir}"
 		mv $PROJ_PATH/.git/ $wpTempDir
-		echo "\nMoving the WordPress project gitignore to ${wpTempDir}"
     mv $PROJ_PATH/.gitignore $wpTempDir
+		echo ">>>DONE!"
   fi
 }
 
@@ -52,6 +53,25 @@ function updateBoilerplate() {
 function updateWP() {
 	echo "\nAwesome! You want to get back to your WordPress project. We're working on it!"
 	echo "If you made a mistake, hit Ctrl-C"
+
+	# checking existence of repo in root
+	if [ -d $BASE_PATH/.git ] || [ -e $BASE_PATH/.gitignore ] ; then
+		echo "\nMoving the boilerplate repo to ${bpTempDir} \n"
+		mv $BASE_PATH/.git $bpTempDir
+		mv $BASE_PATH/.gitignore $bpTempDir
+		echo ">>>DONE!"
+	fi
+
+	# check if the project exists in the temp directory
+	if [ -d $wpTempDir/.git ] || [ -e $wpTempDir/.gitignore ] ; then
+		echo "\nMoving the WordPress project to ${PROJ_PATH} \n"
+		mv $wpTempDir/.git $PROJ_PATH
+		mv $wpTempDir/.gitignore $PROJ_PATH
+		echo ">>>DONE!"
+	fi
+
+	echo "You are all set to work on your WordPress project!"
+
 }
 
 # --------------------------------------------------------
