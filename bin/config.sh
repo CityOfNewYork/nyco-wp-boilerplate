cc396cc90e8cabaf5b809b24c9ffcc098d435f1c
devowhippit <devonhirth@gmail.com>
2019-06-24 11:14:45 -0400

    script base path in config

--- bin/config.sh
+++ bin/config.sh
@@ -1,3 +1,5 @@
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+BASE_PATH=$(dirname "$SCRIPT_PATH")
 
 # source the environmental variables and scripts
 source $BASE_PATH/config/bin.cfg
